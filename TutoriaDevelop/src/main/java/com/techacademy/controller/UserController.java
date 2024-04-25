ba4ecdbdf84af96fa248a3b40255ae1d15c967a5
dir-FY24uchidasoma <SomaUchida()@Dirbato-1388x>
2024-04-25 11:55:34 +0900

    Lesson21

--- TutoriaDevelop/src/main/java/com/techacademy/controller/UserController.java
+++ TutoriaDevelop/src/main/java/com/techacademy/controller/UserController.java
@@ -2,11 +2,17 @@ package com.techacademy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.techacademy.service.UserService;
+import com.techacademy.entity.User;
+import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("user")
@@ -22,5 +28,22 @@ public class UserController {
         //user/list,htmlに画面遷移
         return "user/list";
     }
+    
+    /**User登録画面を表示*/
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        //User登録画面に遷移
+        return "user/register";
+    }
+    
+    /**User登録処理*/
+    @PostMapping("/register")
+    public String  postRegister(User user) {
+        //User登録
+        service.saveUser(user);
+        //一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    
 
 }
--- TutoriaDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutoriaDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,7 @@
 package com.techacademy.service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,11 @@ public class UserService {
     public List<User>getUserList(){
        //リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
+    }
+    
+    /**Userの登録を行う*/
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 }
